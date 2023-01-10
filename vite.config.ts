477c65d09b5fac3fbe30f723de88976bbd178c03
yangjiaxing01 <yangjiaxing01@bilibili.com>
2023-01-10 11:23:37 +0800

    fix: gh pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-tic-tac-toc/',
   build: {
     outDir: 'docs'
   },
