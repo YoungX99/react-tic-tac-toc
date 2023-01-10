3ba19de8845364825921b82063d965698655aa41
yangjiaxing01 <yangjiaxing01@bilibili.com>
2023-01-10 10:59:56 +0800

    fix: the root path supported by gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react(), WindiCSS()]
 })
